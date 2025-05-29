e9c14ef53eab3b0fe9e9312c02d870974cdd37cb
Trip311 <elad.alayoff1@gmail.com>
2025-05-29 10:10:36 +0300

    added delete request in front

--- toran-front/src/Components/layout/ViewReqs/ReqDetails/ReqDetails.tsx
+++ toran-front/src/Components/layout/ViewReqs/ReqDetails/ReqDetails.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import styles from './ReqDetails.module.scss';
 import { ReqDetailsProps } from '../../../../interfaces/reqdetails.props';
-
+import { useAppDispatch } from '../../HomePage/Calendar/redux/hooks';
+import { deleteRequest } from '../../HomePage/Calendar/redux/requestSlice';
 
 
 const ReqDetails: React.FC<ReqDetailsProps> = ({ request }) => {
+
+  const dispatch = useAppDispatch();
+
+  const handleDelete = async () => {
+
+    try {
+      
+      if (request?.id) {
+        await dispatch(deleteRequest(request.id)).unwrap();
+      }
+    } catch (error) {
+        alert('Failed to delete request: ' + (error as Error).message);
+    }
+
+  }
+
+  const handleApprove = () => {
+
+  }
+
+
   return (
     <div className={styles.reqCard}>
       <p className={styles.reqTitle}>
@@ -15,8 +37,8 @@ const ReqDetails: React.FC<ReqDetailsProps> = ({ request }) => {
       <p className={styles.reqDetail}><strong>Reason:</strong> {request.reason}</p>
       
       <div className={styles.reqButtons}>
-        <button className={`${styles.btn} ${styles.approve}`}>Approve</button>
-        <button className={`${styles.btn} ${styles.decline}`}>Decline</button>
+        <button className={`${styles.btn} ${styles.approve}`} onClick={handleApprove}>Approve</button>
+        <button className={`${styles.btn} ${styles.decline}`} onClick={handleDelete}>Decline</button>
       </div>
     </div>
   );
