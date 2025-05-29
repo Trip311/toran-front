7bc0e2b8ac9cbace41765d865d8ed3e206b9e842
elad1122 <elad.alayoff1@gmail.com>
2025-05-29 22:10:30 +0300

    trying new design

--- toran-front/src/Components/layout/Welcome/welcome.tsx
+++ toran-front/src/Components/layout/Welcome/welcome.tsx
@@ -6,33 +6,34 @@ const Welcome: React.FC = () => {
     const navigate = useNavigate();
     const [isOpen, setIsOpen] = useState(false);
 
-    const toggleHelpScreen = () => {
-        setIsOpen(!isOpen);
-    }
+    const toggleHelpScreen = () => setIsOpen(!isOpen);
+
     localStorage.setItem('username', 'guest');
 
     return (
         <div className={styles.container}>
             <button className={styles.btnhelp} onClick={toggleHelpScreen}>?</button>
+
             {isOpen && (
                 <div className={styles.helpscreen}>
                     <div className={styles.helpcontent}>
                         <h2 className={styles.helptitle}>About</h2>
                         <p>The Toran website should help us organize daily jira and kitchen shifts.</p>
-                        <p>Every Signed user in the team could just go into this website and see the status for the week.</p>
-                        <p>In addition, change their shifts accordingly.</p>
+                        <p>Every signed user in the team can visit this website and view the weekly status.</p>
+                        <p>They can also change their shifts accordingly.</p>
                         <p className={styles['superuser-note']}><strong>Only super users can approve switching shifts.</strong></p>
                         <button className={styles.closebutton} onClick={toggleHelpScreen}>X</button>
                     </div>
                 </div>
             )}
+
             <h1 className={styles.title}>Toran</h1>
             <div className={styles.toggleButtons}>
-                <button datatype='btn-login' onClick={() => navigate('/login')}>Login</button>
-                <button datatype='btn-signup' onClick={() => navigate('/signup')}>Signup</button>
+                <button datatype="btn-login" onClick={() => navigate('/login')}>Login</button>
+                <button datatype="btn-signup" onClick={() => navigate('/signup')}>Signup</button>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Welcome
+export default Welcome;
